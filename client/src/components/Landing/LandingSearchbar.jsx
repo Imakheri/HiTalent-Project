4f0c1f7f1f364e2f77ed33c5cd4d8447a00c573e
zantica <santicarrizo@live.com.ar>
2021-11-27 01:28:13 -0300

    Retoque searchbar

--- client/src/components/Landing/LandingSearchbar.jsx
+++ client/src/components/Landing/LandingSearchbar.jsx
@@ -15,14 +15,7 @@ export default function LandingSearchbar() {
         e.preventDefault()
         setSearch(e.target.value)
     }
-    function deleteInput(e) {
-        e.preventDefault()
-        setSearch('')
-    }
-    function changeHidden(e) {
-        e.preventDefault()
-        e.target.hidden = ''
-    }
+
     return (
         <div class="p-3 h-72 bg-light rounded-md m-5 flex flex-col items-center justify-around bg-opacity-70">
             <div class="flex flex-col items-center">
@@ -30,19 +23,14 @@ export default function LandingSearchbar() {
                 <small class="text-2xl">Busca por nuestro sitio y encuentra lo adecuado para ti</small>
             </div>
             <form onSubmit={onSubmit}>
-                <div class=" bg-gray-200">
-                    <div class="container  flex justify-center items-center px-4 sm:px-6 lg:px-8">
-                        <div class="relative"> 
-                            <input onChange={onChange} value={search} required="true" type="text" class="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Prueba con 'cocinar'"/>
-                            <div class="absolute top-4 right-3">
-                                <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <button onClick={deleteInput} onChange={changeHidden} hidden="hidden">X</button>
-                        </div>
+                <div class="searchbar-inner bg-gray-200">
+                        <div class="searchbar-input-wrap container flex justify-center items-center px-4 sm:px-6 lg:px-8">
+                        <input onChange={onChange} type="search" class="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Prueba con 'cocinar'"/> 
+                            <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
                     </div>
                 </div>
             </form>
