f821c797adc6e099e3f91e73b3e21883308c38aa
lambda1158 <brunoherrera1158@gmail.com>
2021-11-29 22:01:37 -0300

    crud post listo

--- api/src/controllers/postLogic.js
+++ api/src/controllers/postLogic.js
@@ -1,4 +1,4 @@
-const {Posts,Users,Category}=require("../db")
+const {Posts,Users,Categories}=require("../db")
 
 const getPosts= async(req,res,next)=>{
     var post=await Posts.findAll()
@@ -6,17 +6,111 @@ const getPosts= async(req,res,next)=>{
 }
 const createPost= async(req,res,next)=>{
     
+    let{title,description,category,duration,cost,username}=req.body
+    let file=req.file
+    let path = "http://localhost:3001/" + file.filename
+    try{
+        var [categoryDB,created]= await Categories.findOrCreate({where:{ title:category}})
+        var post=await Posts.create({
+            title,
+            description,
+            duration:Number(duration),
+            cost:Number(cost),
+            image:[path]
+
+        })
+        var user=await Users.findOne({where:{username}})
+        if(!user)return res.status(500).json({
+            message:"usuario invalido"
+        })
+        post.setCategory(categoryDB)
+        post.setUser(user)
+        res.json(post)
+    }catch(e){
+        res.status(500).json({
+            message:"algo salio mal",
+            error:e.message
+        })
+    }
 }
 const updatePost= async(req,res,next)=>{
-    
+    console.log("update post no imagen")
+    let{title,description,duration,cost,id}=req.body
+    try{
+        var post=await Posts.findByPk(id)
+        if(title)post.title=title
+        if(description)post.description=description
+        if(duration)post.duration=duration
+        if(cost)post.cost=cost
+        post.save()
+        res.json(post)
+    }catch(e){
+        res.status(500).json({
+            message:"algo salio mal",
+            error:e.message
+        })
+    }
 }
 const deletePost= async(req,res,next)=>{
-    
+    let { id } = req.body;
+    try {
+      let existsInDB = await Posts.findByPk(id); // primero busca si existe el user en la DB. Si existe lo guarda en esta variable
+      if (existsInDB) {
+        await Posts.destroy({where: {id}});
+        return res.json(existsInDB); // devuelve el post eliminado como el metodo pop()
+      } else {
+        throw new Error(
+          "ERROR 500: El usuario no fue encontrado en la base de datos (UUID no existe)."
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
 }
+
+const addImage = async(req,res,next)=>{
+    let {id}=req.body
+    let file=req.file
+    let path = "http://localhost:3001/" + file.filename
+    var post=await Posts.findByPk(id)
+    if(!post)res.status(500).json({
+        message:"post no encontrado"
+    })
+    try{
+        post.image=[...post.image,path]
+        post.save()
+        res.json(post)
+    }catch(e){
+        res.status(500).json({
+            message:"algo salio mal",
+            error:e.message})
+    }
+
+}
+const deleteImage = async(req,res,next)=>{
+    var {image,id}=req.body
+    var post=await Posts.findByPk(id)
+    try{
+        
+        post.image=post.image.filter(e=>e!=image)
+        post.save()
+        res.json(post)
+    }catch(e){
+        res.status(500).json({
+            message:"algo salio mal",
+            error:e.message
+        })
+    }
+
+}
+
+
 
 module.exports={
     getPosts,
     createPost,
     updatePost,
-    deletePost
+    deletePost,
+    addImage,
+    deleteImage
 }
--- api/src/routes/routers/post.js
+++ api/src/routes/routers/post.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const router = Router();
-const {getPosts,updatePost,createPost, deletePost}=require("../../controllers/postLogic");
+const {getPosts,updatePost,createPost, deletePost, addImage, deleteImage}=require("../../controllers/postLogic");
 const { uploader } = require("../../middleware/uploader");
 
 router.get("/",getPosts)
 router.post("/",uploader.single("image"),createPost)
-router.put("/",uploader.single("image"),updatePost)
+router.put("/",updatePost)
 router.delete("/",deletePost)
-
-
+router.put("/image",uploader.single("image"),addImage)
+router.delete("/image",deleteImage)
 
 
 module.exports = router;
