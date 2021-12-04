6da5d9ed352cfaf31050d1a01982b097cf7e1462
zantica <santicarrizo@live.com.ar>
2021-12-04 19:17:33 -0300

    Mejorando un poco el CSS, agregue resenas y valoraciones en cada detalle, nombre de usuario

--- client/src/components/SeeMore/Q&A.jsx
+++ client/src/components/SeeMore/Q&A.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Input, Button } from '@chakra-ui/react'
 import { postQuestion } from "../../actions";
 import { useDispatch, useSelector } from "react-redux"
@@ -6,6 +6,8 @@ import { useDispatch, useSelector } from "react-redux"
 export default function QyA() {
     const userState = useSelector(state => state.index.user)
     const dispatch = useDispatch()
+    const [ question, setQuestion ] = useState('')
+    console.log(question)
     
     useEffect(() => {
         dispatch(postQuestion(userState.id))
@@ -16,12 +18,22 @@ export default function QyA() {
         dispatch(postQuestion())
     }
 
+    function onChange(e) {
+        e.preventDefault()
+        setQuestion(e.target.value)
+    }
+
+    function onClick(e) {
+        e.preventDefault()
+        dispatch(postQuestion())
+    }
+
     return (
-        <div>
-            <h3>Deja tu pregunta</h3>
+        <div class="m-3">
+            <h3 class="text-xl font-semibold">Deja tu pregunta</h3>
             <form onSubmit={onSubmit}>
-                <Input placeholder='Ingrese su pregunta' size="md"/>
-                <Button>Enviar</Button>
+                <Input value={question} onChange={onChange} placeholder='Ingrese su pregunta' size="md"/>
+                <Button onClick={onClick}>Enviar</Button>
             </form>
         </div>
     )
