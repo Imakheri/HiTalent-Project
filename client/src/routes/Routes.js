763221e51aac914070f2cc19a6746c300c9f45ec
F-ranco <francooropel00@gmail.com>
2021-12-12 15:37:55 -1200

    add route admin

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -4,18 +4,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { firebase } from "../firebase/firebase-config";
 import { login } from "../actions/auth";
 // import { Private } from './Private';
-import Home from '../components/Home/Home';
-import Landing from '../components/Landing/Landing';
-import Validate from '../components/Register/Validate';
-import Register from '../components/Register/Register';
-import TalentForm from '../components/Talents/TalentForm';
-import SeeMore from '../components/SeeMore/SeeMore';
-import Profile from '../components/Profile/Profile';
-import ResetPassword from '../components/ResetPassword/ResetPassword';
-import EmailResetPassword from '../components/ResetPassword/EmailResetPassword';
-import Faq from '../components/Faq/Faq';
-import AdminProfile from '../components/AdminProfile/AdminProfile';
-import ShoppingCart from '../components/ShoppingCart/ShoppingCart';
+import Home from "../components/Home/Home";
+import Landing from "../components/Landing/Landing";
+import Validate from "../components/Register/Validate";
+import Register from "../components/Register/Register";
+import TalentForm from "../components/Talents/TalentForm";
+import SeeMore from "../components/SeeMore/SeeMore";
+import Profile from "../components/Profile/Profile";
+import ResetPassword from "../components/ResetPassword/ResetPassword";
+import EmailResetPassword from "../components/ResetPassword/EmailResetPassword";
+import Faq from "../components/Faq/Faq";
+import AdminProfile from "../components/AdminProfile/AdminProfile";
+import ShoppingCart from "../components/ShoppingCart/ShoppingCart";
 import CheckoutMP from "../components/Checkout/CheckoutMP";
 import ProfilePublic from "../components/ProfilePublic/ProfilePublic";
 import Messenger from "../components/Chat/Messenger";
@@ -38,10 +38,10 @@ export const Rout = () => {
     });
   }, [dispatch, checking, isLoggedIn]);
 
-// No se para que es esto
-//   if (checking) {
-//     return <h3>Cargando...</h3>;
-//   }
+  // No se para que es esto
+  //   if (checking) {
+  //     return <h3>Cargando...</h3>;
+  //   }
 
   return (
     <BrowserRouter>
@@ -73,10 +73,11 @@ export const Rout = () => {
         <Route path="/talent/:id" element={<SeeMore />} />
         <Route path="/checkoutApro" element={<CheckoutMP />} />
         <Route path="/messenger" element={<Messenger />} />
-        <Route path='/faq' element={<Faq/>} />
-        <Route path='/cart' element={<ShoppingCart/>} />
-        <Route path='/about' element={<AboutUs/>} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/about" element={<AboutUs />} />
         {/* <ProtectedRoute isLoggedIn={isLoggedIn} path='/home' element={<Home />} /> */}
+        <Route path="/admin" element={<AdminProfile />} />
       </Routes>
     </BrowserRouter>
   );
